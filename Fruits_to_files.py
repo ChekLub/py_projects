9482c264d6afef1c7f190bba910e4442c42287ba
ChekLub <76036839+ChekLub@users.noreply.github.com>
2023-01-01 22:13:51 +0300

    Add files via upload

---
+++ Fruits_to_files.py
@@ -0,0 +1,25 @@
+import os
+
+path = os.path.join('data', 'fruits.txt')
+f = open(path, 'r', encoding="UTF-8")
+#alfavit = list(map(chr, range(ord('А'), ord("Я")+1)))
+
+try:
+    os.mkdir("Fruits")
+except FileExistsError:
+    print('Директория уже существеут')
+
+for fruit in f:
+    char = fruit[0]
+
+    if char != '\n':
+        file_name = 'Fruits_' + char + '.txt'
+        lst_Fruits = os.listdir("Fruits")
+        path_fruits = os.path.join('Fruits', file_name)
+        if file_name in lst_Fruits:
+            f_wr = open(path_fruits, 'a', encoding='UTF-8')
+        else:
+            f_wr = open(path_fruits, 'w', encoding='UTF-8')
+        f_wr.write(fruit)
+        f_wr.close()
+
